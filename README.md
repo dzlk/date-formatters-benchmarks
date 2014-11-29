f2ea39a9282f432a18d6ff284e5cab2ff6ab98f8
dzlk <dlb.diesel@gmail.com>
2014-11-29 19:53:26 +0500

    Add benchmarks for datef@0.0.5 and dateformat@1.0.11

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-date-formatters-benchmarks
-==========================
+Benchmarks for date formatters
+==============================
+
+Run benchmarks:
+```javascript
+npm i
+npm run bench
+```
+
+The result will be like this:
+```
+                      datef@0.0.5
+         534,208 op/s » dd.MM
+         338,920 op/s » dd MMMM YYYY
+         460,113 op/s » dd.MM.YY
+         221,024 op/s » DD, MMMM dd, YYYY, h:MM:ss TT
+
+                      dateformat@1.0.11
+         506,913 op/s » dd.mm
+         456,238 op/s » dd MMMM YYYY
+         511,334 op/s » dd.MM.YY
+         317,697 op/s » dddd, mmmm dd, yyyy, h:MM:ss TT
+```
---
+++ benchmark/index.js
@@ -0,0 +1,45 @@
+/*global suite, bench */
+'use strict';
+
+suite('datef@0.0.5', function() {
+  var formatter = require('datef');
+
+  bench('dd.MM', function() {
+    formatter('dd.MM');
+  });
+
+  bench('dd MMMM YYYY', function() {
+    formatter('dd MMMM YYYY');
+  });
+
+  bench('dd.MM.YY', function() {
+    formatter('dd.MM.YY');
+  });
+
+  bench('DD, MMMM dd, YYYY, h:MM:ss TT', function() {
+    formatter('DD, MMMM dd, YYYY, h:mm:ss A');
+  });
+
+});
+
+
+suite('dateformat@1.0.11', function() {
+  var formatter = require('datef');
+
+  bench('dd.mm', function() {
+    formatter('dd.mm');
+  });
+
+  bench('dd MMMM YYYY', function() {
+    formatter('dd mmmm yyyy');
+  });
+
+  bench('dd.MM.YY', function() {
+    formatter('dd.mm.yy');
+  });
+
+  bench('dddd, mmmm dd, yyyy, h:MM:ss TT', function() {
+    formatter('dddd, mmmm dd, yyyy, h:MM:ss TT');
+  });
+
+});
